6bef57039bc6226de2cf66020b5841218c1a5131
Finishitgolf <seandormedit@gmail.com>
2026-07-03 11:58:39 -0700

    Delete Buy_or_Sell_Golf_App_PWA directory

--- Buy_or_Sell_Golf_App_PWA/service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open('buy-sell-v1').then(cache => cache.addAll([
-    './','index.html','styles.css','app.js','manifest.json','icon.svg'
-  ])));
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
-});
